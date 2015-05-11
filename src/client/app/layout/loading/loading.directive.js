7a77d360617399777d47a5f9694e0f447d661b6e
Stoltz <Stoltzy409@gmail.com>
2015-05-11 16:25:06 -0400

    Dabbling around with directives for loading

---
+++ src/client/app/layout/loading/loading.directive.js
@@ -0,0 +1,9 @@
+angular.module('app.loading').directive('zsLoading', function () {
+  'use strict';
+
+  return {
+    restrict: 'E',
+    templateUrl: '../app/layout/loading/loading.html',
+    scope: {}
+  };
+});
